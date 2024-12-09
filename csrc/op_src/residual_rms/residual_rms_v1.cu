55ff1dba446409ccd166efc62ba62c58cb27d85c
remi-or <remi.pierre_o@orange.fr>
2024-12-09 22:31:02 +0000

    Added resdiual_rms V1

--- csrc/op_src/residual_rms/residual_rms_v1.cu
+++ csrc/op_src/residual_rms/residual_rms_v1.cu
@@ -31,7 +31,7 @@ __global__ void _residual_rms_v1(const half* __restrict__ input, half* __restric
         for (int j = 0; j < WPT; j++) {
             input_buffer[j] = input[i + j];
         }
-        #pragma unroll 
+        #pragma unroll
         for (int j = 0; j < WPT; j++) {
             residual_buffer[j] = residual[i + j];
         }
@@ -65,12 +65,17 @@ __global__ void _residual_rms_v1(const half* __restrict__ input, half* __restric
 
     // Normalize and convert
     float tmp_float;
+    half residual_buffer_[WPT];
     half weight_buffer[WPT];
     __hip_fp8_storage_t fp8_buffer[WPT];
 
     for (int i = WPT * threadIdx.x; i < cols; i += WPT * blockDim.x) {
 
-        // 128-bits load
+        // 128-bits loads
+        #pragma unroll 
+        for (int j = 0; j < WPT; j++) {
+            residual_buffer_[j] = residual[i + j];
+        }
         #pragma unroll 
         for (int j = 0; j < WPT; j++) {
             weight_buffer[j] = weight[i + j];
@@ -79,8 +84,8 @@ __global__ void _residual_rms_v1(const half* __restrict__ input, half* __restric
         // Compute and fill buffer
         #pragma unroll 
         for (int j = 0; j < WPT; j++) {
-            tmp_float = (float)residual[i + j];
-            tmp_float = (float)((half)(tmp_float * shared_normalizer) * weight[i + j]);
+            tmp_float = (float)residual_buffer_[j] * shared_normalizer;
+            tmp_float = (float)((half)(tmp_float) * weight_buffer[j]);
             tmp_float *= scale;
             FP8_CLAMP(tmp_float, float);
             fp8_buffer[j] = __hip_cvt_float_to_fp8(tmp_float, __HIP_SATFINITE, __HIP_E4M3_FNUZ);
