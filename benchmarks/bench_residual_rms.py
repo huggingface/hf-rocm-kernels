16faf1148d406fb232bd3f08d2321299d5b97d6d
remi-or <remi.pierre_o@orange.fr>
2024-12-09 14:11:21 +0000

    Added benchmark of residual_rms

---
+++ benchmarks/bench_residual_rms.py
@@ -0,0 +1,27 @@
+from tqdm import tqdm
+
+from hf_rocm_kernels.operators.residual_rms import residual_rms, generate_residual_rms_data, reference_residual_rms
+from hf_rocm_kernels.utils.benchmark import Bench
+
+
+if __name__ == "__main__":
+    bench = Bench()
+
+    list_rows = [1, 2, 4, 8, 16, 32, 64, 128, 256]
+    cols = 16384
+
+    for rows in tqdm(list_rows, "Gathering reference times"):
+        args = generate_residual_rms_data(rows, cols)
+        bench.add_measure(
+            header="Ref (μs)", 
+            label=rows, 
+            fn=lambda: reference_residual_rms(*args),
+        )
+        for mode in [0]:
+            bench.add_measure(
+                header=f"Mode {mode} (μs)", 
+                label=rows, 
+                fn=lambda: residual_rms(*args[:-1], args[-1].item(), mode=mode),
+            )
+
+    bench.display_table(row_header="Nb. rows")
