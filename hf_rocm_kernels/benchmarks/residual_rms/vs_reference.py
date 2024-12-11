b992d3c5096b59603be043c3e08cf1c934c54bc2
remi-or <remi.pierre_o@orange.fr>
2024-12-11 09:58:47 +0000

    Modified gitignore and added roadmap

--- hf_rocm_kernels/benchmarks/residual_rms/vs_reference.py
+++ hf_rocm_kernels/benchmarks/residual_rms/vs_reference.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     bench = Bench()
 
     list_rows = [1, 2, 4, 8, 16, 32, 64, 128, 256]
-    cols = 16384
+    cols = 6656 # to imitate Llama3.1 405B in TP8
 
     for rows in tqdm(list_rows, "Gathering reference times"):
         args = generate_residual_rms_data(rows, cols)
