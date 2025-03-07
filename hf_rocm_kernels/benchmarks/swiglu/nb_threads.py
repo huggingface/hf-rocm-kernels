16ee3a3711eeda69af8fafbfc85db8dea3e36907
remi-or <remi.pierre_o@orange.fr>
2025-03-07 14:54:08 +0000

    [Misc] Nb threads revamp

--- hf_rocm_kernels/benchmarks/swiglu/nb_threads.py
+++ hf_rocm_kernels/benchmarks/swiglu/nb_threads.py
@@ -1,21 +1,22 @@
-from tqdm import tqdm
-import torch
+
 import os
 import matplotlib.pyplot as plt
 import argparse
+import torch
 from typing import List
-from hf_rocm_kernels.operators.residual_rms import residual_rms, generate_residual_rms_data
+from tqdm import tqdm
+from hf_rocm_kernels.operators.swiglu import swiglu, generate_swiglu_data
 from hf_rocm_kernels.utils.benchmarking import Bench
 
 try:
-    from hf_rocm_kernels.benchmarks.residual_rms.vs_vllm import get_vllm_time
+    from hf_rocm_kernels.benchmarks.swiglu.vs_vllm import get_vllm_time
 except ImportError:
     get_vllm_time = lambda *args, **kwargs: 0
 
 
 # Parameters
-COLS = 16384
-BUFFER_COLS = 13312
+INTERMEDIATE_SIZE = 6656  # to imitate Llama3.1 405B in TP8
+BUFFER_COLS = 16384
 DTYPE = torch.float8_e4m3fnuz
 
 
@@ -25,8 +26,8 @@ def draw_nb_threads_plot(list_rows: List[int]) -> None:
         # Gather measures
         ns, ts = [], []
         for nthreads in tqdm([64 * i for i in range(1, 17)]):
-            args = generate_residual_rms_data(rows, COLS, BUFFER_COLS, DTYPE)
-            t = bench.benchmark_fn(fn=lambda: residual_rms(*args, nthreads))
+            gate_up_proj, scale_tensor, next_buffer = generate_swiglu_data(rows, INTERMEDIATE_SIZE, BUFFER_COLS)
+            t = bench.benchmark_fn(fn=lambda: swiglu(gate_up_proj, scale_tensor, next_buffer, num_threads=nthreads))
             ns.append(nthreads)
             ts.append(t)
         # Find the minimum time
@@ -35,19 +36,17 @@ def draw_nb_threads_plot(list_rows: List[int]) -> None:
         # Plot the curve
         plt.plot(ns, ts, label=f"{rows=} w/ min=({min_n}, {min_t:.2f})")
         # Add VLLM's time if available
-        vllm_time = get_vllm_time(bench, rows, COLS, BUFFER_COLS, DTYPE)
+        vllm_time = get_vllm_time(bench, rows, INTERMEDIATE_SIZE)
         if vllm_time > 0:
-            plt.axhline(vllm_time, color="red", linestyle="--", label=f"VLLM w/ {rows =}")
+            plt.axhline(vllm_time, color="red", linestyle="--", label=f"VLLM w/ {rows}")
         plt.legend()
         plt.savefig("__bench__.png")
 
 
 if __name__ == "__main__":
-    bench = Bench()
-
-    # Parse arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--rows", "-r", nargs="+", type=int, default=[1, 2, 4, 8, 16, 32, 64, 128, 256, 1024, 2048])
+    parser = argparse.ArgumentParser(description="Benchmark swiglu operation with different thread counts")
+    parser.add_argument( "--rows", "-r", type=int, nargs="+", default=[1, 2, 4, 8, 16, 32, 64, 128, 256, 1024, 2048])
     args = parser.parse_args()
-
+    
     draw_nb_threads_plot(args.rows)
+
