ded1547ec4f485bfee12709fa3dbe0adef4d1915
remi-or <remi.pierre_o@orange.fr>
2025-04-14 14:49:02 +0000

    [Misc] More clang

--- csrc/ops.h
+++ csrc/ops.h
@@ -14,18 +14,6 @@ void residual_rms(torch::Tensor& input, torch::Tensor& residual, torch::Tensor& 
 void swiglu(torch::Tensor& gate_up, torch::Tensor& scale_tensor, torch::Tensor& output, torch::Tensor& next_buffer,
             int64_t num_threads, bool force_scalar);
 
-int64_t skinny_gemm_tb(
-    torch::Tensor& A,
-    torch::Tensor& B,
-    torch::Tensor& D,
-    torch::Tensor& scale_tensor,
-    int64_t split_k,
-    int64_t A_producers,
-    int64_t B_producers,
-    int64_t consumers,
-    int64_t a_lanes,
-    int64_t b_lanes,
-    int64_t qsize,
-    int64_t op_m,
-    int64_t ops
-);
+int64_t skinny_gemm_tb(torch::Tensor& A, torch::Tensor& B, torch::Tensor& D, torch::Tensor& scale_tensor,
+                       int64_t split_k, int64_t A_producers, int64_t B_producers, int64_t consumers, int64_t a_lanes,
+                       int64_t b_lanes, int64_t qsize, int64_t op_m, int64_t ops);
--- csrc/torch_bindings.cpp
+++ csrc/torch_bindings.cpp
@@ -35,8 +35,9 @@ TORCH_LIBRARY_EXPAND(TORCH_EXTENSION_NAME, ops) {
     ops.impl("swiglu", torch::kCUDA, &swiglu);
 
     // Skinny GEMM
-    ops.def("skinny_gemm_tb(Tensor A, Tensor B, Tensor! D, Tensor scale_tensor, int split_k, int A_producers, "
-            "int B_producers, int consumers, int a_lanes, int b_lanes, int qsize, int op_m, int ops) -> int");
+    ops.def(
+        "skinny_gemm_tb(Tensor A, Tensor B, Tensor! D, Tensor scale_tensor, int split_k, int A_producers, "
+        "int B_producers, int consumers, int a_lanes, int b_lanes, int qsize, int op_m, int ops) -> int");
     ops.impl("skinny_gemm_tb", torch::kCUDA, &skinny_gemm_tb);
 }
 
